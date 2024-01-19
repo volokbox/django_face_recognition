8a9fe357f243419973d4018278cb7d60ebd37c53
volokbox <118136472+volokbox@users.noreply.github.com>
2024-01-19 16:30:58 +0000

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,11 @@
 # pages/urls.py
 from django.contrib import admin
 from django.urls import path
-from pages.views import face_recognition
+from pages.views import face_recognition, take_photos, train_photos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('face_recognition/', face_recognition, name='face_recognition'),
-
+    path('take_photos/', take_photos, name='take_photos'),
+    path('train_photos/', train_photos, name='train_photos'),
 ]
